ae9e22e5b215bc12b92759c40366d29e6a90176f
AVasilaki <poseydon463@gmail.com>
2024-12-20 21:13:49 +0200

    moove minute

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const minute = document.querySelector(".minute");
+const minuteShadow = document.querySelector(".shadow-minute");
+const hour = document.querySelector(".hour");
+const hourShadow = document.querySelector(".shadow-hour");
+console.log(minute, hour);
 
+setInterval(() => {
+  const currentTime = new Date();
+  //   console.log(currentTime);
+  const currentMinute = currentTime.getMinutes();
+  const currentHour = currentTime.getHours();
+  const rotateMinute = (360 / 60) * currentMinute + 55;
+  const rotateShadowMinute = (360 / 60) * currentMinute + 57;
+  const rotateHour = (360 / 60) * currentHour;
+  console.log(rotateMinute);
+  minute.style.transform = `rotate(${rotateMinute}deg)`;
+  minuteShadow.style.transform = `rotate(${rotateShadowMinute}deg)`;
+}, 60000);
